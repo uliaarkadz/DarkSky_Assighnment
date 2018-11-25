1dd88c32c3639b451418a6b60f30e7a8023c9f01
ulia <juliabuiko@gmail.com>
2018-11-24 23:33:54 -0500

    first commit to branch FA2-86

---
+++ src/test/java/framework/DarkSkyLandingPage.java
@@ -0,0 +1,76 @@
+
+
+package framework;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import ru.yandex.qatools.allure.annotations.Step;
+
+public class DarkSkyLandingPage extends BasePage {
+    private By searchField = By.xpath("//input[@type='text']");
+    private By magniFyingGlass = By.xpath("//a[@class='searchButton']");
+    private By currentTemperature = By.xpath("//span[@class='summary swap']");
+    private By lowTemperature = By.xpath("//span[@class='currently']//span[2]//span[2]//span[2]");
+    private By highTemperature = By.xpath("//span[@class='summary-high-low']//span[3]");
+    private By timeLine = By.xpath("//span[@class='hour']//descendant::*");
+    private By todaysTimeline = By.xpath("//body[@class='forecast']/div[@id='week']/a[1]/span[3]/span[1]/*[1]");
+    private By lowTempToday = By.xpath("/html[1]/body[1]/div[6]/a[1]/span[2]/span[1]");
+    private By highTempToday = By.xpath("/html[1]/body[1]/div[6]/a[1]/span[2]/span[3]");
+
+
+    @Step
+    public void clickOnSearchField() {
+        this.clickOn(this.searchField);
+    }
+
+    public void clearSearchField() {
+        this.clearTextField(this.searchField);
+    }
+
+    public void enterZipCode(String zip) {
+        this.sendText(this.searchField, zip);
+    }
+
+    public void clickOnMagnifyingGlass() {
+        this.clickOn(this.magniFyingGlass);
+    }
+
+    public void verifyCurrentTemperature() {
+        try {
+            Thread.sleep(5000L);
+        } catch (InterruptedException var4) {
+            var4.printStackTrace();
+        }
+
+        int temp = this.converTextIntoInt(this.currentTemperature, 0, 2);
+        int tempLow = this.converTextIntoInt(this.lowTemperature, 5, 7);
+        int tempHigh = this.converTextIntoInt(this.highTemperature, 6, 8);
+        if (temp >= tempLow && temp <= tempHigh) {
+            System.out.println("The current temperature is between low and high value");
+        } else {
+            Assert.fail("The current temperature is above high value");
+        }
+
+    }
+
+    public void verifyTimeline(int hour) {
+        Assert.assertEquals(this.getCurrentTime(hour), this.getHoursFromTimeline(this.timeLine), "The element are not equal");
+        System.out.println("Timeline is displayed with 2 hours incremented");
+    }
+
+    public void expandTodaysTimeline() throws InterruptedException {
+        Thread.sleep(4000L);
+        scrollOnThePage();
+        this.clickOn(this.todaysTimeline);
+    }
+
+    public void verifyTodaysTimelineTemp() {
+        int tempLowTimeline = this.converTextIntoInt(this.lowTemperature, 5, 7);
+        int tempHighTimeline = this.converTextIntoInt(this.highTemperature, 6, 8);
+
+        int tempLowToday = this.converTextIntoInt(this.lowTempToday, 0, 2);
+        int tempHighToday = this.converTextIntoInt(this.highTempToday, 0, 2);
+
+        Assert.assertTrue(tempLowToday == tempLowTimeline || tempHighTimeline == tempHighToday, "Lowest and highest temp is displayed correctly");
+    }
+}
